9626d3ece7c9d791b35d44bf433c3713dfd74dab
taylor <tchen@bespokemetrics.com>
2017-07-11 15:42:40 -0400

    updated message

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-rm "api_test.txt" 2> /dev/null
-
-echo "auth_api DOWN" >> "api_test.txt"
-
-echo "auth_api DOWN" >> "api_test.txt"
-
-[ -e api_test.txt ] && exit 1
+echo "****************************************"
+echo "auth_api DOWN"
+echo "user_api DOWN"
+echo "****************************************"
